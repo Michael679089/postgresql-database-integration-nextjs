a942288fb0e9c4b2457d12063a7a5ce0421cdbd8
Michael <90962435+Michael679089@users.noreply.github.com>
2025-06-21 15:17:17 +0800

    npm build 4

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 Hello I'm testing this git
-
-Hola I'm Kim Jong Un!
-
-WHAT THE FUCK
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
-// src/app/page.tsx
 export default function Home() {
-  return <h1>Hello from Home Page</h1>;
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      Ola ola ola ola
+    </div>
+  );
 }
